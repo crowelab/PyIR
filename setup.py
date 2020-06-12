6c8cf1edcd8582731448ceec7e458a750d217f3c
crowelab <sambday@vvcenter.org>
2020-06-12 13:01:20 -0500

    Update setup.py
    Update PyIR version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyir',
-    version='1.3.0',
+    version='1.3.1',
     description='',
     author='Sam Day, Andre Branchizio, Jordan Willis, Jessica Finn, Taylor Jones, Sam Schmitz',
     author_email='samuel.day@vumc.org, andrejbranch@gmail.com, jwillis0720@gmail.com, strnad.bird@gmail.com',
